1e1969be5dabc8473cb56b867c38cec28fba33b8
Sam Burnstone <sburnstone@scottlogic.com>
2016-08-11 14:24:32 +0100

    Adds project

---
+++ 03 - Xcode Extensions/Emojificator/Emojificator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Emojificator
+//
+//  Created by Samuel Burnstone on 10/08/2016.
+//  Copyright © 2016 ShinobiControls. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ 03 - Xcode Extensions/Test_Project.swift
@@ -0,0 +1,16 @@
+//
+//  Test_Project.swift
+//  
+//
+//  Created by Samuel Burnstone on 10/08/2016.
+//
+//
+
+import Foundation
+
+
+/// This is a test class ;)
+class Test {
+    /// Performs a method that does something with the value pi and then returns it :)
+    func performTask() {}
+}
