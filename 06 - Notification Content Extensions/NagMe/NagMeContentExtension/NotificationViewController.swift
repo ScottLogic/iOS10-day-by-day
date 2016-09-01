bd51b2bf6c4b64476312d529b7e88c0302de6a02
Sam Burnstone <sburnstone@scottlogic.com>
2016-09-01 17:29:43 +0100

    Add speaker image outlet and shake

--- 06 - Notification Content Extensions/NagMe/NagMeContentExtension/NotificationViewController.swift
+++ 06 - Notification Content Extensions/NagMe/NagMeContentExtension/NotificationViewController.swift
@@ -12,7 +12,8 @@ import UserNotificationsUI
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
-    @IBOutlet var label: UILabel?
+    @IBOutlet var label: UILabel!
+    @IBOutlet weak var speakerLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +21,21 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
     }
     
     func didReceive(_ notification: UNNotification) {
-        self.label?.text = "Reminder: \(notification.request.content.body)"
+        label.text = "Reminder: \(notification.request.content.body)"
+        speakerLabel.shake()
     }
 
+}
+
+extension UIView {
+    
+    // Very slightly modified version of sample code found in stack overflow post http://stackoverflow.com/a/34778432
+    func shake() {
+        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.duration = 1
+        animation.repeatCount = .infinity
+        animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
+        layer.add(animation, forKey: "shake")
+    }
 }
