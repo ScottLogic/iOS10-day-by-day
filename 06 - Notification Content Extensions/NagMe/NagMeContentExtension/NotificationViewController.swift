2412a9f284685645ad2abd4903624e642bedc6fd
Sam Burnstone <sburnstone@scottlogic.com>
2016-09-02 10:07:15 +0100

    Add cancel shake method to UIView extension

--- 06 - Notification Content Extensions/NagMe/NagMeContentExtension/NotificationViewController.swift
+++ 06 - Notification Content Extensions/NagMe/NagMeContentExtension/NotificationViewController.swift
@@ -38,4 +38,8 @@ extension UIView {
         animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
         layer.add(animation, forKey: "shake")
     }
+    
+    func cancelShake() {
+        layer.removeAnimation(forKey: "shake")
+    }
 }
