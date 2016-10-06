7716059a2d8395fdbaa1d84df6555de634eb5d81
Sam Burnstone <sam.burnstone@gmail.com>
2016-10-06 09:14:24 +0100

    Base project for Bike Taxi
    This is an app name Siri can reliably understand... tried fancier names,
    but was going to be a frustrating day if I didn't choose something
    simple!

---
+++ 09 - Siri Intents/Bike Taxi/Bike Taxi/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Bike Taxi
+//
+//  Created by Sam Burnstone on 06/10/2016.
+//  Copyright © 2016 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ 09 - Siri Intents/Bike Taxi/Bike Taxi/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Bike Taxi
+//
+//  Created by Sam Burnstone on 06/10/2016.
+//  Copyright © 2016 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
